86313f46116b8cbb1882b699ddcd7bf0f9a48cc4
gabioliveira666 <105853751+gabioliveira666@users.noreply.github.com>
2025-05-31 08:35:50 -0300

    Removida necessidade de rodar node server.js manualmente

--- backend/pagamento-service/publisher.js
+++ backend/pagamento-service/publisher.js
@@ -2,7 +2,7 @@ const amqp = require('amqplib');
 
 async function publicarPagamento(pagamento) {
     try {
-        const connection = await amqp.connect('amqp://localhost');
+        const connection = await amqp.connect('amqp://rabbitmq');
         const channel = await connection.createChannel();
         const queue = 'pagamentos';
 
